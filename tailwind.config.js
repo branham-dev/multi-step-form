5d66f6e97bac92053a93bb371ccf297d78c31876
Branham Karanja <branham.karanja@outlook.com>
2025-07-28 14:28:35 +0300

    feat: add custom HSL color palette for Tailwind theme

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+export default {
+	content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				marine: "hsl(213, 96%, 18%)",
+				purplish: "hsl(243, 100%, 62%)",
+				pastel: "hsl(228, 100%, 84%)",
+				lightblue: "hsl(206, 94%, 87%)",
+				strawberry: "hsl(354, 84%, 57%)",
+				gray: {
+					cool: "hsl(231, 11%, 63%)",
+					light: "hsl(229, 24%, 87%)",
+				},
+				magnolia: "hsl(217, 100%, 97%)",
+				alabaster: "hsl(231, 100%, 99%)",
+				white: "hsl(0, 0%, 100%)",
+			},
+		},
+	},
+	plugins: [],
+};
